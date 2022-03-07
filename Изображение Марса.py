33836017ee987c34246a77cfb153aba3055596e0
Dententr <60317826+dententr118@users.noreply.github.com>
2022-03-07 18:35:25 +0300

    Update Изображение Марса.py

--- Изображение Марса.py
+++ Изображение Марса.py
@@ -35,10 +35,38 @@ def mars_image():
                   <body>
                     <h1>Жди нас, Марс!</h1>
                   <body>
-                    <img src="/static/img/riana.jpg" alt="здесь должна была быть картинка, но не нашлась">
+                    <img src="/static/img/mars.png" alt="здесь должна была быть картинка, но не нашлась">
                   <body>
                     <br>Вот она какая, красная планета.<br>
             '''
+
+
+@app.route("/promotion_image")
+def promotion_image():
+    return f'''<!doctype html>
+                    <html lang="en">
+                      <head>
+                        <meta charset="utf-8">
+                        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+                        <link rel="stylesheet" 
+                    href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" 
+                    integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" 
+                    crossorigin="anonymous">
+                        <link rel="stylesheet" type="text/css" href="{url_for('static', filename='css/style.css')}" />
+
+                        <title>Привет, Марс!</title>
+                      </head>
+                      <body>
+                        <h1>Жди нас, Марс!</h1>
+                        <img src="/static/img/mars.png" alt="здесь должна была быть картинка, но не нашлась">
+                        <p style="font-size: 120%; font-family: monospace; color: #cd66cc">Вот она какая, красная планета.</p>
+                        <p style="font-size: 120%; font-family: monospace; color: #ff0000">Человечество вырастает из детства.</p>
+                        <p style="font-size: 120%; font-family: monospace; color: #008000">Человечеству мала одна планета.</p>
+                        <p style="font-size: 120%; font-family: monospace; color: #0000ff">Мы сделаем обитаемыми безжизненные пока планеты.</p>
+                        <p style="font-size: 120%; font-family: monospace; color: #8b00ff">И начнем с Марса!</p>
+                        <p style="font-size: 120%; font-family: monospace; color: #ffff00">Присоединяйся!</p>
+                      <body>
+                '''
 
 
 if __name__ == '__main__':
